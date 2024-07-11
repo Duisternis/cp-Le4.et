4bb57b102835b1481248c43b8c417cf4316793ad
Varnan Matela <88179333+Duisternis@users.noreply.github.com>
2024-07-11 18:18:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int l=0, r=nums.size()-1, m;
+        while (l<=r) {
+            m=l+(r-l)/2;
+            if (nums[m]==target) return true;
+
+            if (nums[m]==nums[l] && nums[m]==nums[r]) {
+                l++;
+                r--;
+                continue;
+            }
+
+            if (nums[l] <= nums[m]) {
+                if (nums[l]<=target && nums[r]>=target) r=m-1;
+                else l=m+1;
+            } else {
+                if (nums[m]<=target && nums[r]>=target) l=m+1;
+                else r=m-1;
+            }
+        }
+
+        return false;
+    }
+};
