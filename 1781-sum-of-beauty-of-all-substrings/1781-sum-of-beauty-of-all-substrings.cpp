45dfa30e99858e93dc81dd921bfbedb3b4c9f254
Varnan Matela <88179333+Duisternis@users.noreply.github.com>
2024-08-14 00:05:32 +0530

    Time: 126 ms (79.08%), Space: 26.2 MB (37.93%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
     int beautySum(string s) {
-        if (s=="knlzzhnfltqakxnlixytpvaqlqevyrprgxiorsvkapjmlcrwcekputnzaxxffeqqqzvtdcldoeyqbehsfztsopjqzujzekbinwfkoeajszactvxutjqeldcccmxlgdunkimvlxiykgsxtchhtshrmacrpiqiqfftchsznupfxzcaedpcxnbysyacwzlumpbvzptqtrzojiqscjfggylwoxchjxjcylukbjikyidfiqxqtsaqpfabuenbmdmrcirfmzcnhjpfetjitpomjwixgcqcjjquqfpkhmggpsnlacnubytpytjvjovurlciewbumgnjaemivtehdmoshrhjeeogowcuetxhkivtawhlttajodbwdvhcctbeikcokxouvvgtiayzitevaazkovujcssbesgyihbitzxsauuyssaduengrkhdhwoshdcdvxdrkewnyismpvrxqfo") return 972131;
         int i, j, r=0, cmax, cmin;
-        unordered_map<char, int> f;
-        vector<unordered_map<char, int>> pf;
-
-        // prefix freq
-        for (char ch : s) {
-            f[ch]++;
-            pf.push_back(f);
-        }
-
-        // traversing substr
-        for (i=0; i<s.size(); ++i) 
-            for (j=i+3; j<=s.size(); ++j) {
-                pf[j-1][s[i]]++;
-                cmax=0; cmin=1e9;
-                // cout << i << " " << j-1 << "_____" << endl;
-                for (auto el : pf[j-1]) {
-                    cmax=max(cmax, el.second-pf[i][el.first]);
-                    if (el.second-pf[i][el.first])
-                        cmin=min(cmin, el.second-pf[i][el.first]);
-                    // cout << el.second << "-" << pf[i][el.first] << "|" << el.first << endl;
+        for (i=0; i<s.size(); ++i) {
+            unordered_map<char, int> f;
+            for (j=i; j<s.size(); ++j) {
+                f[s[j]]++;
+                cmax=INT_MIN;
+                cmin=INT_MAX;
+                for (auto el : f) {
+                    cmax=max(cmax, el.second);
+                    cmin=min(cmin, el.second);
                 }
-                
                 r+=cmax-cmin;
-                // cout << cmax << " == " << cmin << endl;
-                pf[j-1][s[i]]--;
             }
-
+        }
         return r;
     }
 };
