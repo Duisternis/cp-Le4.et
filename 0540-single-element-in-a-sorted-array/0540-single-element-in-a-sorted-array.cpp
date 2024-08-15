b2e3a10acbe9e8e11fa88ee5a3ae814c7b541694
Varnan Matela <88179333+Duisternis@users.noreply.github.com>
2024-08-15 17:50:58 +0530

    Time: 21 ms (40.57%), Space: 24.9 MB (30.5%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -4,8 +4,7 @@ public:
         int l=0, r=nums.size()-1, m;
         while (l<=r) {
             m=l+(r-l)/2;
-            if (r==l) break;
-            if (nums[m]!=nums[m-1] && nums[m]!=nums[m+1]) break;
+            if (r==l || (nums[m]!=nums[m-1] && nums[m]!=nums[m+1])) break;
             if (((r-l-1)/2+1)%2==0) {
                 if (nums[m+1]==nums[m]) l=m+2;
                 else r=m-2;
